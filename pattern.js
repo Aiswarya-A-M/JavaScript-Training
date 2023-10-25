a320d254825c06666a05ca1921c3417e682ee1ad
Aiswarya-A-M <148937898+Aiswarya-A-M@users.noreply.github.com>
2023-10-25 22:03:51 +0530

    Add files via upload

--- pattern.js
+++ pattern.js
@@ -1,26 +1,32 @@
+/* if input is 5 we have to print above pattern
+  *
+ ***
+*****
+ ***
+  *  
+*/
 function pattern(num){
-    for(let i=1;i<=num;i+=2){
-       let str='';
-       let space=(num-i)/2
-       for (let j=1;j<=space;j++){
-          str+=" ";
-       }
-       for (let j=1;j<=i;j++){
-            str+="*";
-       }
-             console.log(str)
-    }
-      for(let i=num-2;i>0;i-=2){
-       let str='';
-       let space=(num-i)/2
-       for (let j=1;j<=space;j++){
-          str+=" ";
-       }
-       for (let j=1;j<=i;j++){
-            str+="*";
-       }
-             console.log(str)
-    }
-    
- }
- let f=pattern(11)
+   for(let i=1;i<=num;i+=2){
+      let str='';
+      const space=(num-i)/2
+      for (let j=1;j<=space;j++){
+         str+=" ";
+      }
+      for (let j=1;j<=i;j++){
+         str+="*";
+      }
+      console.log(str)
+   }
+   for(let i=num-2;i>0;i-=2){
+      let str='';
+      const space=(num-i)/2
+      for (let j=1;j<=space;j++){
+         str+=" ";
+      }
+      for (let j=1;j<=i;j++){
+         str+="*";
+      }
+      console.log(str)
+   }
+}
+pattern(11)
--- problem_solving.js
+++ problem_solving.js
@@ -1,85 +1,66 @@
 //1) sort this array using inbuilt functions
-function sort(a){
-    a.sort();
-    console.log("sorted array using inbuilt function",a);
-
-
+function sort(array){                                          //function used for sorting given array.
+  array.sort();
+  console.log("sorted array using inbuilt function",array);
 }
-let array = [3, 0, 4, 2, 0, 5, 8, 5, 8, 9, 6, 6, 4, 0];
-let a=sort(array);
-//2)remove duplicates of the array using inbuilt functions
-function removeDuplicates(b){
-    let setOfB=new Set(b);
-    let arrayOfB=Array.from(setOfB);
-    console.log("removed duplicates from array using inbuilt function",arrayOfB)
+sort([3, 0, 4, 2, 0, 5, 8, 5, 8, 9, 6, 6, 4, 0]);
 
-
+//2)remove duplicates from the array using inbuilt functions
+function removeDuplicates(array){                                //function used for removing duplicates from given array using in-built functions. 
+  const setOfArray=new Set(array);
+  const arrayOfSet=Array.from(setOfArray);
+  console.log("removed duplicates from array using inbuilt function",arrayOfSet)
 }
-let ar= [3, 0, 4, 2, 0, 5, 8, 5, 8, 9, 6, 6, 4, 0];
-let b=removeDuplicates(ar);
+removeDuplicates([3, 0, 4, 2, 0, 5, 8, 5, 8, 9, 6, 6, 4, 0]);
+
 //3) sort this array without inbuilt functions using basic loops and all.
-function sorting(arr){
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-             if (arr[j] > arr[j + 1]) {
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-              }
-       }
-   }
+function sortAnArray(arr){                                 // funtion used for sort an array without using inbuilt functions.
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
   return arr;
 }
-let c=[3, 0, 4, 2, 0, 5, 8, 5, 8, 9, 6, 6, 4, 0];
-let sortedArray=sorting(c);
-console.log("sorted array without inbuilt functions using basic loops and all.",sortedArray);
+console.log("sorted array without inbuilt functions using basic loops and all.",sortAnArray([3, 0, 4, 2, 0, 5, 8, 5, 8, 9, 6, 6, 4, 0]));
 
 //4) remove duplicates of the array with normal loops
-
-
-
-function duplicate(d){
-    let newArray=[];
-    for (let i=0;i<d.length;i++){
-      if(newArray.indexOf(d[i])===-1){
-             newArray.push(d[i]);
-      }
-   }
-   return newArray;
+function toRemoveDuplicate(arr){      //function used for removing duplicates from given array without using in-built functions.
+  let newArray=[];
+  for (let i=0;i<arr.length;i++){
+    if(newArray.indexOf(arr[i])===-1){
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
 }
-let d=duplicate([3, 0, 4, 2, 0, 5, 8, 5, 8, 9, 6, 6, 4, 0]);
+console.log("duplicates removed array with normal loops",toRemoveDuplicate([3, 0, 4, 2, 0, 5, 8, 5, 8, 9, 6, 6, 4, 0]));
 
-
-
-
-console.log("duplicates removed array with normal loops",d);
 //5) Find the leader of the array
-
-
-function leader(arr){
-     const n= arr.length;
-     let max=arr[n-1];
-     const maxArray=[max];
-     for (let i=n-2;i>0;i--){
-         if(arr[i]>max){
-            max=arr[i];
-            maxArray.push(arr[i]);
-         }
-     
-     }
-   return maxArray;
+function leader(arr){                   //function used for getting array of leaders in a given array.
+  const length= arr.length;
+  let right=arr[length-1];
+  const leadArray=[right];
+  for (let i=length-2;i>0;i--){
+    if(arr[i]>right){
+      right=arr[i];
+      leadArray.push(arr[i]);
+    }  
+  }
+  return leadArray;
 }
-let e=[16, 17, 4, 3, 5, 2];
-console.log("leaders of given array",leader(e));
-
+console.log("leaders of given array",leader([16, 17, 4, 3, 5, 2]));
 
 //6) Rearrange an array in maximum minimum form
-function maxMin(arr) {
-  let n = arr.length;
+function maxMin(arr) {              // function is used for rearrange an array in maximum minimum form.Array should be in the format of [1,2,3,4,5,6] 
+  const length = arr.length;
   let left = 0;
-  let right = n - 1;
+  let right = length - 1;
   let newArray = [];
-
   while (left <= right) {
     if (left === right) {
       newArray.push(arr[left]);
@@ -87,38 +68,29 @@ function maxMin(arr) {
       newArray.push(arr[right]);
       newArray.push(arr[left]);
     }
-
     left++;
     right--;
   }
-
   return newArray;
 }
-
-let maxMinArray = maxMin([1, 2, 3, 4, 5, 6]);
-console.log("Rearranged an array in maximum-minimum form:", maxMinArray);
+console.log("Rearranged an array in maximum-minimum form:",maxMin([1, 2, 3, 4, 5, 6]));
 
 //7) In the above array move all zeroes to end of array
-
-function zeroAtRight(arr){
+function zeroAtRight(arr){                // function to get an array with all zeroes placed at end
   let left = 0;
   let right=arr.length-1;
   let temp;
-
   while (left<right){
-     if(arr[left]===0){
-         const temp=arr[left];
-         arr[left]=arr[right];
-         arr[right]=temp;
-         right--;
-   }   
-   else{
+    if(arr[left]===0){
+      temp=arr[left];
+      arr[left]=arr[right];
+      arr[right]=temp;
+      right--;
+    }   
+    else{
       left++;
-   }
+    }
   }
   return arr;
 }
-const arr = [3, 0, 4, 2, 0, 5, 8, 5, 8, 9, 6, 6, 4, 0];
-const result = zeroAtRight(arr);
-console.log("Array with zeroes moved to the end: ",result);
-
+console.log("Array with zeroes moved to the end: ",zeroAtRight([3, 0, 4, 2, 0, 5, 8, 5, 8, 9, 6, 6, 4, 0]));

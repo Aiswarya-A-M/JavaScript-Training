5e750282ec1a53eafb4273bb541e5d34334c2a6d
Aiswarya-A-M <aiswarya.am@neoito.com>
2023-11-25 21:43:22 +0530

    formatted

--- combinationPattern.js
+++ combinationPattern.js
@@ -5,30 +5,50 @@
 */
 
 function generatePattern(n) {
-  let result = [];
-  let arrayOfCombination = [];
-
-  function combination(openBracketNumber, closedBracketNumber) {
-    if (openBracketNumber === closedBracketNumber && openBracketNumber === n) {
-      result.push(arrayOfCombination.join(""));
-      return;
-    }
-
-    if (openBracketNumber < n) {                                      //open bracket will move to array if number of elements in array are less than total number.
-      arrayOfCombination.push("(");
-      combination(openBracketNumber + 1, closedBracketNumber);
-      arrayOfCombination.pop();
-    }
-
-    if (closedBracketNumber < openBracketNumber) {                   //closed bracket will move to array if number of closed brackets are less than open brackets.
-      arrayOfCombination.push(")");
-      combination(openBracketNumber, closedBracketNumber + 1);
-      arrayOfCombination.pop();
-    }
-  }
-  combination(0, 0);
+  const result = [];
+  const arrayOfCombination = [];
+  combination(0, 0, arrayOfCombination, result, n);
   return result;
 }
 
-let pattern = generatePattern(3);
+function combination(
+  openBracketNumber,
+  closedBracketNumber,
+  arrayOfCombination,
+  result,
+  n
+) {
+  if (openBracketNumber === closedBracketNumber && openBracketNumber === n) {
+    result.push(arrayOfCombination.join(""));
+    return;
+  }
+
+  if (openBracketNumber < n) {
+    //open bracket will move to array if number of elements in array are less than total number.
+    arrayOfCombination.push("(");
+    combination(
+      openBracketNumber + 1,
+      closedBracketNumber,
+      arrayOfCombination,
+      result,
+      n
+    );
+    arrayOfCombination.pop();
+  }
+
+  if (closedBracketNumber < openBracketNumber) {
+    //closed bracket will move to array if number of closed brackets are less than open brackets.
+    arrayOfCombination.push(")");
+    combination(
+      openBracketNumber,
+      closedBracketNumber + 1,
+      arrayOfCombination,
+      result,
+      n
+    );
+    arrayOfCombination.pop();
+  }
+}
+
+const pattern = generatePattern(3);
 console.log(pattern);
